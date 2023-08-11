d14712291633feebfb068a2adc25be8e1e8ac020
nooralu <76510310+nooralu@users.noreply.github.com>
2023-08-11 07:21:23 +0000

    配置 jwt

--- server/config/config.default.ts
+++ server/config/config.default.ts
@@ -37,6 +37,13 @@ export default (appInfo: EggAppInfo) => {
     },
   };
 
+  // jwt
+  config.jwt = {
+    secret: 'qwertyuiopzxcvbnmasdfghjkl',
+    // 一星期
+    expiresIn: '7d',
+  };
+
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
